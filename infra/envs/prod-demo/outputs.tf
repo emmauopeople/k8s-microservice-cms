da35c65d28daadfa18ab17cece0b4c0043f0d5d5
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-06-30 15:56:08 -0500

    feat: add prod-demo vpc outputs

--- infra/envs/prod-demo/outputs.tf
+++ infra/envs/prod-demo/outputs.tf
@@ -1,15 +1,35 @@
-
 output "ecr_repository_names" {
-  description = "ECR repository names."
-  value       = module.ecr.repository_names
+  value = module.ecr.repository_names
 }
 
 output "ecr_repository_urls" {
-  description = "ECR repository URLs."
-  value       = module.ecr.repository_urls
+  value = module.ecr.repository_urls
 }
 
 output "ecr_repository_arns" {
-  description = "ECR repository ARNs."
-  value       = module.ecr.repository_arns
+  value = module.ecr.repository_arns
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_app_subnet_ids" {
+  value = module.vpc.private_app_subnet_ids
+}
+
+output "private_db_subnet_ids" {
+  value = module.vpc.private_db_subnet_ids
+}
+
+output "nat_gateway_ids" {
+  value = module.vpc.nat_gateway_ids
 }
