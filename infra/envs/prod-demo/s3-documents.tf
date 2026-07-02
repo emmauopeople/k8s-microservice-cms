1014e0722bfa88fcbe09adda4f8fe261584a06cb
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-07-02 17:04:50 -0500

    feat: wire document bucket module into prod-demo

---
+++ infra/envs/prod-demo/s3-documents.tf
@@ -0,0 +1,16 @@
+module "s3_documents" {
+  source = "../../modules/s3-documents"
+
+  name          = "${local.name_prefix}-documents"
+  force_destroy = var.document_bucket_force_destroy
+
+  versioning_enabled = true
+
+  cors_allowed_origins = var.document_bucket_cors_allowed_origins
+  cors_allowed_methods = ["GET", "HEAD", "PUT", "POST"]
+
+  noncurrent_version_expiration_days        = 30
+  abort_incomplete_multipart_upload_days    = 7
+
+  tags = local.common_tags
+}
