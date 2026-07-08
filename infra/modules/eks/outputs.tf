a35e318740915650820d98f2f263f9610647b1fe
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-07-08 14:19:44 -0500

    feat: expose eks oidc issuer for irsa

--- infra/modules/eks/outputs.tf
+++ infra/modules/eks/outputs.tf
@@ -19,6 +19,11 @@ output "cluster_certificate_authority_data" {
   sensitive   = true
 }
 
+output "cluster_oidc_issuer_url" {
+  description = "EKS cluster OIDC issuer URL used for IRSA."
+  value       = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
 output "cluster_security_group_id" {
   description = "EKS cluster security group ID."
   value       = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
