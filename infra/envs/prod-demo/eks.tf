fd2bfa3ba74c8e6274624b77aaf8d07c2d07a4cb
emmauopeople <mbimunyui_uopeople@yahoo.com>
2026-07-08 13:42:30 -0500

    feat: wire eks module into prod-demo

---
+++ infra/envs/prod-demo/eks.tf
@@ -0,0 +1,28 @@
+module "eks" {
+  source = "../../modules/eks"
+
+  cluster_name       = var.cluster_name
+  kubernetes_version = var.eks_kubernetes_version
+
+  cluster_subnet_ids = module.vpc.private_app_subnet_ids
+  node_subnet_ids    = module.vpc.private_app_subnet_ids
+
+  endpoint_private_access = var.eks_endpoint_private_access
+  endpoint_public_access  = var.eks_endpoint_public_access
+  public_access_cidrs     = var.eks_public_access_cidrs
+
+  enabled_cluster_log_types   = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  cluster_log_retention_days  = 7
+
+  node_group_name     = "system"
+  node_instance_types = var.eks_node_instance_types
+  node_capacity_type  = var.eks_node_capacity_type
+  node_desired_size   = var.eks_node_desired_size
+  node_min_size       = var.eks_node_min_size
+  node_max_size       = var.eks_node_max_size
+  node_disk_size      = var.eks_node_disk_size
+
+  cluster_addons = ["vpc-cni", "kube-proxy", "coredns"]
+
+  tags = local.common_tags
+}
